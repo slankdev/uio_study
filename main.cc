a251662d6bc2e1b4438c57b1b063e82935b3657a
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-10-12 21:59:57 +0900

    2017.10.12-21:59

--- main.cc
+++ main.cc
@@ -2,13 +2,15 @@
 
 #include <stdio.h>
 #include <slankdev/intfd.h>
+using slankdev::intfd;
+using slankdev::exception;
 constexpr uint16_t INTEL = 0x8086;
 constexpr uint16_t IXGBE_X540 = 0x1528;
 
 int main()
 {
-  slankdev::intfd uio_fd("/dev/uio0", O_RDONLY);
-  slankdev::intfd configfd("/sys/class/uio/uio0/device/config", O_RDWR);
+  intfd uio_fd("/dev/uio0", O_RDONLY);
+  intfd configfd("/sys/class/uio/uio0/device/config", O_RDWR);
   printf("configfd: %d \n", configfd.get_fd());
 
   uint16_t vendor_id;
@@ -16,7 +18,7 @@ int main()
   configfd.pread(&vendor_id, 2, 0);
   configfd.pread(&device_id, 2, 2);
   if (!(vendor_id==INTEL && device_id==IXGBE_X540))
-    throw slankdev::exception("device is not X540!!!");
+    throw exception("device is not X540!!!");
 
   uint32_t bar;
   configfd.pread(&bar, 4, 0x10);
@@ -29,7 +31,7 @@ int main()
   size = ~size + 1;
   configfd.pwrite(&bar, 4, 0x10);
 
-  slankdev::intfd fd("/sys/class/uio/uio0/device/resource0", O_RDWR);
+  intfd fd("/sys/class/uio/uio0/device/resource0", O_RDWR);
   void* addr = fd.mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, 0);
   printf("addr: %p \n", addr);
 }
